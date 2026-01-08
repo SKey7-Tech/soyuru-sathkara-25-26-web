0d25def7e4ff3a365f9f97b373297c79fbbbd015
PamothKumarasinghe <nikolailaazz2@gmail.com>
2026-01-08 23:48:32 +0530

    icons changed: favicon not displayed

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,9 @@ export const metadata: Metadata = {
   keywords: ["GCE O/L", "Sri Lanka", "education", "learning resources", "papers", "video lectures", "theory notes", "uom", "University of Moratuwa"],
   authors: [{ name: "Soyuru Sathkara", url: "https://ss.efsu-uom.lk" }],
   publisher: "Soyuru Sathkara",
-  icons: {
-    icon: "/favicon.ico",
-  },
+  // icons: {
+  //   icon: "/favicon.ico",
+  // },
   alternates: {
     canonical: "https://ss.efsu-uom.lk",
     languages: {
